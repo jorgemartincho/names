e9da9a4981788b2bc9d06b608eb4560e2372a8a9
Unknown <jorgemartincho@gmail.com>
2018-04-16 01:10:43 -0300

    save in local storage

--- names.js
+++ names.js
@@ -1,13 +1,26 @@
-$(document).ready(function(){
-	$(function() {
-	  $(".hideshow").click(function(args) {
-		var showStuff = $(this).prop("checked");
-		$(this).closest(".autohide").each(function() {
-		  if (showStuff)
-			$(this).addClass("checkhidden");
-		  else
-			$(this).removeClass("checkhidden");
-		});
-	  });
-	});
+$(document).ready(function() {
+  $(function() {
+	  
+    $(".hideshow").click(function(args) {
+      var showStuff = $(this).prop("checked");
+      $(this).closest(".autohide").each(function() {
+        if (showStuff) {
+          $(this).addClass("checkhidden");
+          localStorage.setItem($(this).attr('id'), "checkhidden");
+        } else {
+          $(this).removeClass("checkhidden");
+        }
+      });
+    });
+
+    $(".autohide").each(function() {
+
+      var checkhidden = localStorage.getItem($(this).find('input').attr('id'), "checkhidden");
+      if (checkhidden === "checkhidden") {
+        alert(1);
+        $(this).closest(".autohide").addClass("checkhidden");
+      }
+    });
+	
+  });
 });
