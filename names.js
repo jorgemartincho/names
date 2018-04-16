77cfca290656934804b99ae36fa7d7615850a601
Unknown <jorgemartincho@gmail.com>
2018-04-16 01:17:29 -0300

    Fix save

--- names.js
+++ names.js
@@ -1,26 +1,27 @@
 $(document).ready(function() {
   $(function() {
-	  
     $(".hideshow").click(function(args) {
       var showStuff = $(this).prop("checked");
       $(this).closest(".autohide").each(function() {
         if (showStuff) {
           $(this).addClass("checkhidden");
-          localStorage.setItem($(this).attr('id'), "checkhidden");
+          localStorage.setItem($(this).find('ínput').attr('id'), "checkhidden");
         } else {
           $(this).removeClass("checkhidden");
         }
       });
     });
 
+
     $(".autohide").each(function() {
 
       var checkhidden = localStorage.getItem($(this).find('input').attr('id'), "checkhidden");
       if (checkhidden === "checkhidden") {
-        alert(1);
         $(this).closest(".autohide").addClass("checkhidden");
       }
     });
-	
+
+
+
   });
 });
